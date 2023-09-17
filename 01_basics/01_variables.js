4534601ad10221fe32fe713acca1c939b228641c
GANDHI KARAN B <41875208+210798@users.noreply.github.com>
2023-09-17 17:17:52 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=144553;
+let accountEmail="karan@google.com";
+var accountpPassword ="12345";
+accountCity="Baroda";
+
+// accountId=2;
+accountEmail="karan@gmail.com";
+console.log("accountId-----",accountId);
+console.table([accountId,accountEmail,accountpPassword,accountCity]);
