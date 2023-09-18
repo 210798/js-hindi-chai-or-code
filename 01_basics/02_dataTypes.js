e8f2ec8551d4c1d1f74d6a5fd0351fe9583e3bb1
GANDHI KARAN B <41875208+210798@users.noreply.github.com>
2023-09-18 16:00:40 +0000

    Date

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,10 @@
 
 // alert("karan")  //we are using node.js not browser
 
+let mydate = new  Date();
+console.log("DATE1",mydate);
+console.log("DATE2",mydate.toDateString());
+console.log("DATE3",mydate.toISOString());
+console.log("DATE4",mydate.toJSON());
+console.log("DATE5",mydate.toLocaleDateString());
+console.log("DATE6",mydate.toLocaleTimeString());
